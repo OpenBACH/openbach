0fb5334f138a1ebd763f0c504ed35b7f73cdb012
Adrien Thibaud <adrien.thibaud@toulouse.viveris.com>
2016-03-23 15:50:29 +0100

    Install of Controller, Collector and Agents

--- openbach_add_agent.sh
+++ openbach_add_agent.sh
@@ -1,13 +1,60 @@
 #!/bin/bash
 
 read -p "Agent Ip Address : ? " agent_address
-read -p "SSH Agent Username : ? " agent_username
-read -s -p "SSH Agent Password : ? " agent_password
+if [ -z $agent_address ]
+then
+    echo "You should provide at least one IP address"
+    exit
+fi
+echo "[Agents]" > configs/hosts_controller
+while [ ! -z $agent_address ]
+do
+    echo "$agent_address" >> configs/hosts_controller
+    read -p "Agent Ip Address : ? " agent_address
+done
+read -p "SSH Agents Username : ? " agent_username
+if [ -z $agent_username ]
+then
+    echo "You should provide the username of the Agents"
+    exit
+fi
+read -s -p "SSH Agents Password : ? " agent_password
 echo
-read -p "SSH Username : ? " username
-read -s -p "SSH Password : ? " password
+if [ -z $agent_password ]
+then
+    echo "You should provide the password of the Agents"
+    exit
+fi
+read -p "Collector Ip Address : ? " collector_address
+if [ -z $collector_address ]
+then
+    echo "You should provide the IP address of the Controller"
+    exit
+fi
+read -p "Controller Ip Address : ? " controller_address
+if [ -z $controller_address ]
+then
+    echo "You should provide the IP address of the Controller"
+    exit
+fi
+read -p "SSH Controller Username : ? " username
+if [ -z $username ]
+then
+    echo "You should provide the username of the Controller"
+    exit
+fi
+read -s -p "SSH Controller Password : ? " password
 echo
+if [ -z $password ]
+then
+    echo "You should provide the password of the Controller"
+    exit
+fi
+echo -e "[Controller]\n$controller_address\n" > configs/hosts
+echo "collector_ip: $collector_address" > configs/ips
 
 
-ansible-playbook -i configs/hosts -e ansible_ssh_user=$username -e ansible_sudo_pass=$password -e ansible_ssh_pass=$password -e agent_address=agent_address -e agent_username=agent_username -e agent_password=agent_password install/agent.yml --tags add_agent
+ansible-playbook -i configs/hosts -e ansible_ssh_user=$username -e ansible_sudo_pass=$password -e ansible_ssh_pass=$password -e agent_username=$agent_username -e agent_password=$agent_password install/agent.yml --tags add_agent
+
+rm configs/hosts_controller configs/hosts configs/ips
 
--- openbach_install.sh
+++ openbach_install.sh
@@ -1,9 +1,52 @@
 #!/bin/bash
 
-read -p "SSH Username : ? " username
-read -s -p "SSH Password : ? " password
+read -p "Controller Ip Address : ? " controller_address
+if [ -z $controller_address ]
+then
+    echo "You should provide the IP address of the Controller"
+    exit
+fi
+read -p "SSH Controller Username : ? " controller_username
+if [ -z $controller_username ]
+then
+    echo "You should provide the username of the Controller"
+    exit
+fi
+read -s -p "SSH Controller Password : ? " controller_password
 echo
+if [ -z $controller_password ]
+then
+    echo "You should provide the password of the Controller"
+    exit
+fi
+read -p "Collector Ip Address : ? " collector_address
+if [ -z $collector_address ]
+then
+    echo "You should provide the IP address of the Collector"
+    exit
+fi
+read -p "SSH Collector Username : ? " collector_username
+if [ -z $collector_username ]
+then
+    echo "You should provide the username of the Collector"
+    exit
+fi
+read -s -p "SSH Collector Password : ? " collector_password
+echo
+if [ -z $collector_password ]
+then
+    echo "You should provide the password of the Collector"
+    exit
+fi
+
+echo -e "[Controller]\n$controller_address\n" > configs/hosts
+echo "controller_ip: $controller_address" > configs/ips
+echo -e "[Collector]\n$collector_address" >> configs/hosts
+echo "collector_ip: $collector_address" >> configs/ips
 
 
-ansible-playbook -i configs/hosts -e @configs/all -e ansible_ssh_user=$username -e ansible_sudo_pass=$password -e ansible_ssh_pass=$password install/site.yml --tags install
+ansible-playbook -i configs/hosts -e @configs/ips -e @configs/all -e ansible_ssh_user=$controller_username -e ansible_sudo_pass=$controller_password -e ansible_ssh_pass=$controller_password install/controller.yml --tags install
+ansible-playbook -i configs/hosts -e @configs/ips -e @configs/all -e ansible_ssh_user=$collector_username -e ansible_sudo_pass=$collector_password -e ansible_ssh_pass=$collector_password install/collector.yml --tags install
+
+rm configs/hosts configs/ips
 

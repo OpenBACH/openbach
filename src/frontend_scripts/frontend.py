be275780528164e0c4708ee4d8c81255df1bf782
Mathias Ettinger <mathias.ettinger@toulouse.viveris.com>
2016-05-27 09:52:21 +0200

    Simplified frontend actions

--- src/frontend_scripts/frontend.py
+++ src/frontend_scripts/frontend.py
@@ -6,221 +6,170 @@
 frontend.py - <+description+>
 """
 
-import requests, json
-from datetime import datetime
-import time
-from calendar import timegm
+# PEP8: each import on its own line
+import requests
+import json
+import datetime
+from functools import partial
 
 
-def date_to_timestamp(date):
-    t = time.localtime()
-    utc_offset = timegm(t) - timegm(time.gmtime(time.mktime(t)))
-    date_object = datetime.strptime(date, '%Y-%m-%d %H:%M:%S.%f')
-    timestamp = (date_object - datetime(1970,1,1)).total_seconds() - utc_offset
-    return timestamp
+_URL = "http://localhost:8000/{}/{}/"
+
+
+def _post_message(entry_point, verb, **kwargs):
+    """Helper function to format a request and send it to
+    the right URL.
+    """
+
+    url = _URL.format(entry_point, verb)
+
+    return requests.post(url, data={'data': json.dumps(kwargs)})
+
+
+def date_to_timestamp(date, fmt='%Y-%m-%d %H:%M:%S.%f'):
+    # Simplified function
+    return datetime.datetime.strptime(date, fmt).timestamp()
 
 
 def add_agent(agent_ip, collector_ip, username, password, name):
-    url = "http://localhost:8000/agents/add"
-
-    payload = {'address': agent_ip, 'username': username, 'password':
-               password, 'collector': collector_ip, 'name': name}
-    
-    return requests.post(url, data={'data': json.dumps(payload)})
+    return _post_message('agents', 'add',
+            address=agent_ip, username=username,
+            password=password, collector=collector_ip,
+            name=name)
 
 
 def add_job(job_name, path):
-    url = "http://localhost:8000/jobs/add"
-
-    payload = {'name': job_name, 'path': path}
-    
-    return requests.post(url, data={'data': json.dumps(payload)})
+    return _post_message('jobs', 'add', name=job_name, path=path)
 
 
 def del_agent(agent_ip):
-    url = "http://localhost:8000/agents/del"
-
-    payload = {'address': agent_ip}
-    
-    return requests.post(url, data={'data': json.dumps(payload)})
+    return _post_message('agents', 'del', address=agent_ip)
 
 
 def del_job(job_name):
-    url = "http://localhost:8000/jobs/del"
-
-    payload = {'name': job_name}
-    
-    return requests.post(url, data={'data': json.dumps(payload)})
+    return _post_message('jobs', 'del', name=job_name)
 
 
 def get_job_help(job_name):
-    url = "http://localhost:8000/jobs/help"
-
-    payload = {'name': job_name}
-    
-    return requests.post(url, data={'data': json.dumps(payload)})
+    return _post_message('jobs', 'help', name=job_name)
 
 
 def install_jobs(jobs_name, agents_ip):
-    url = "http://localhost:8000/jobs/install"
-
-    payload = {'addresses': agents_ip, 'names': jobs_name}
-    
-    return requests.post(url, data={'data': json.dumps(payload)})
+    return _post_message('jobs', 'install',
+            names=job_names, addresses=agents_ip)
 
 
 def list_agents(update=None):
-    url = "http://localhost:8000/agents/list"
+    action = _post_message
+    if update is not None:
+        action = partial(action, update=update)
 
-    payload = {}
-    if update != None:
-        payload['update'] = update
-
-    return requests.post(url, data={'data': json.dumps(payload)})
+    return action('agents', 'list')
 
 
 def list_installed_jobs(agent_ip, update=None):
-    url = "http://localhost:8000/jobs/list"
+    action = partial(_post_message, address=agent_ip)
+    if update is not None:
+        action = partial(action, update=update)
 
-    payload = {'address': agent_ip}
-    if update != None:
-        payload['update'] = update
-
-    return requests.post(url, data={'data': json.dumps(payload)})
+    return action('jobs', 'list')
 
 
 def list_instances(agents_ip, update=False):
-    url = "http://localhost:8000/instances/list"
-    
-    payload = {'addresses': agents_ip}
+    action = partial(_post_message, addresses=agents_ip)
     if update:
-        payload['update'] = True
-    
-    return requests.post(url, data={'data': json.dumps(payload)})
+        action = partial(action, update=True)
+
+    return action('instances', 'list')
 
 
 def list_jobs():
-    url = "http://localhost:8000/jobs/list"
-
-    return requests.get(url)
+    return requests.get(_URL.format('jobs', 'list'))
 
 
 def push_file(local_path, remote_path, agent_ip):
-    url = "http://localhost:8000/file/push"
-    
-    payload = {'local_path': local_path, 'remote_path': remote_path, 'agent_ip':
-               agent_ip}
-    
-    return requests.post(url, data={'data': json.dumps(payload)})
+    return _post_message('file', 'push',
+            local_path=local_path, remote_path=remote_path,
+            agent_ip=agent_ip)
 
 
 def restart_instance(instance_id, arguments=None, date=None, interval=None):
-    url = "http://localhost:8000/instances/restart"
+    action = partial(_post_message, instance_id=instance_id,
+            instance_args=[] if arguments is None else arguments)
+    if interval is not None:
+        action = partial(action, interval=interval)
+    if date is not None:
+        action = partial(action, date=date)
 
-    payload = {'instance_id': instance_id}
-    if arguments == None:
-        payload['instance_args'] = list()
-    else:
-        payload['instance_args'] = arguments
-    if interval != None:
-        payload['interval'] = interval
-    elif date != None:
-        payload['date'] = date
-    
-    return requests.post(url, data={'data': json.dumps(payload)})
+    return action('instances', 'restart')
 
 
 def start_instance(agent_ip, job_name, arguments=None, date=None,
                    interval=None):
-    url = "http://localhost:8000/instances/start"
+    action = partial(_post_message, agent_ip=agent_ip, job_name=job_name,
+            instance_args=[] if arguments is None else arguments)
+    if interval is not None:
+        action = partial(action, interval=interval)
+    if date is not None:
+        action = partial(action, date=date)
 
-    payload = {'agent_ip': agent_ip, 'job_name': job_name}
-    if arguments == None:
-        payload['instance_args'] = list()
-    else:
-        payload['instance_args'] = arguments
-    if interval != None:
-        payload['interval'] = interval
-    elif date != None:
-        payload['date'] = date
-    
-    return requests.post(url, data={'data': json.dumps(payload)})
+    return action('instances', 'start')
 
 
 def status_agents(agents_ip):
-    url = "http://localhost:8000/agents/status"
-    
-    payload = {'addresses': agents_ip}
-        
-    return requests.post(url, data={'data': json.dumps(payload)})
+    return _post_message('agents', 'status', addresses=agents_ip)
 
 
 def status_instance(instance_id, date=None, interval=None, stop=None, agent_ip=None,
                     job_name=None):
-    url = "http://localhost:8000/instances/status"
+    action = partial(_post_message, instance_id=instance_id)
+    if interval is not None:
+        action = partial(action, interval=interval)
+    if date is not None:
+        action = partial(action, date=date)
+    if stop is not None:
+        action = partial(action, stop=stop)
+    if agent_ip is not None and job_name is not None:
+        action = partial(action, agent_ip=agent_ip, job_name=job_name)
 
-    payload = {'instance_id': instance_id}
-    if interval != None:
-        payload['interval'] = interval
-    elif date != None:
-        payload['date'] = date
-    elif stop != None:
-        payload['stop'] = stop
-    if agent_ip != None and job_name != None:
-        payload['agent_ip'] = agent_ip
-        payload['job_name'] = job_name
-        
-    return requests.post(url, data={'data': json.dumps(payload)})
+    return action('instances', 'status')
 
 
 def status_jobs(agents_ip):
-    url = "http://localhost:8000/jobs/status"
-
-    payload = {'addresses': agents_ip}
-
-    return requests.post(url, data={'data': json.dumps(payload)})
+    return _post_message('jobs', 'status', addresses=agents_ip)
 
 
 def stop_instance(instance_ids, date=None):
-    url = "http://localhost:8000/instances/stop"
+    action = partial(_post_message, instance_ids=instance_ids)
+    if date is not None:
+        action = partial(action, date=date)
 
-    payload = {'instance_ids': instance_ids}
-    if date != None:
-        payload['date'] = date
-    
-    return requests.post(url, data={'data': json.dumps(payload)})
+    return action('instances', 'stop')
 
 
 def uninstall_jobs(jobs_name, agents_ip):
-    url = "http://localhost:8000/jobs/uninstall"
+    return _post_message('jobs', 'uninstall',
+            addresses=agents_ip, names=jobs_name)
 
-    payload = {'addresses': agents_ip, 'names': jobs_name}
-    
-    return requests.post(url, data={'data': json.dumps(payload)})
 
 def update_job_log_severity(agent_ip, job_name, severity, local_severity=None,
                             date=None):
-    url = "http://localhost:8000/jobs/log_severity"
-    
-    payload = {'address': agent_ip, 'job_name': job_name, 'severity': severity}
-    if local_severity != None:
-        payload['local_severity'] = local_severity
-    if date != None:
-        payload['date'] = date
-    
-    return requests.post(url, data={'data': json.dumps(payload)})
+    action = partial(_post_message, address=agent_ip,
+            job_name=job_name, severity=severity)
+    if local_severity is not None:
+        action = partial(action, local_severity=local_severity)
+    if date is not None:
+        action = patial(action, date=date)
+
+    return action('jobs', 'log_severity')
 
 
 def update_job_stat_policy(agent_ip, job_name, accept_stats, deny_stats,
                            default_policy=True, date=None):
-    url = "http://localhost:8000/jobs/stat_policy"
+    action = partial(_post_message, address=agent_ip,
+            job_name=job_name, accept_stats=accept_stats,
+            deny_stats=deny_stats, default_policy=default_policy)
+    if date is not None:
+        action = partial(action, date=date)
 
-    payload = {'address': agent_ip, 'job_name': job_name, 'accept_stats':
-               accept_stats, 'deny_stats': deny_stats, 'default_policy':
-               default_policy}
-    if date != None:
-        payload['date'] = date
-
-    return requests.post(url, data={'data': json.dumps(payload)})
-
-
+    return action('jobs', 'stat_policy')

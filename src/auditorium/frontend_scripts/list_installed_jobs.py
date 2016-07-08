4442ba1ec25b30fa33e750a46ecd7b008252678d
Adrien Thibaud <adrien.thibaud@toulouse.viveris.com>
2016-07-08 17:17:36 +0200

     Header with license added to the files

--- src/auditorium/frontend_scripts/list_installed_jobs.py
+++ src/auditorium/frontend_scripts/list_installed_jobs.py
@@ -1,10 +1,39 @@
+""" 
+   OpenBACH is a generic testbed able to control/configure multiple
+   network/physical entities (under test) and collect data from them. It is
+   composed of an Auditorium (HMIs), a Controller, a Collector and multiple
+   Agents (one for each network entity that wants to be tested).
+   
+   
+   Copyright © 2016 CNES
+   
+   
+   This file is part of the OpenBACH testbed.
+   
+   
+   OpenBACH is a free software : you can redistribute it and/or modify it under the
+   terms of the GNU General Public License as published by the Free Software
+   Foundation, either version 3 of the License, or (at your option) any later
+   version.
+   
+   This program is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY, without even the implied warranty of MERCHANTABILITY or FITNESS
+   FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+   details.
+   
+   You should have received a copy of the GNU General Public License along with
+   this program. If not, see http://www.gnu.org/licenses/.
+   
+   
+   
+   @file     list_installed_jobs.py
+   @brief    Call the openbach-function list_installed_jobs
+   @author   Adrien THIBAUD <adrien.thibaud@toulouse.viveris.com>
+"""
+
+
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Author: Adrien THIBAUD / <adrien.thibaud@toulouse.viveris.com>
-
-"""
-list_installed_jobs.py - <+description+>
-"""
 
 import argparse
 from frontend import list_installed_jobs, pretty_print
--- src/auditorium/frontend_scripts/update_job_stat_policy.py
+++ src/auditorium/frontend_scripts/update_job_stat_policy.py
@@ -1,9 +1,38 @@
-#!/usr/bin/env python3
-# Author: Adrien THIBAUD / <adrien.thibaud@toulouse.viveris.com>
+""" 
+   OpenBACH is a generic testbed able to control/configure multiple
+   network/physical entities (under test) and collect data from them. It is
+   composed of an Auditorium (HMIs), a Controller, a Collector and multiple
+   Agents (one for each network entity that wants to be tested).
+   
+   
+   Copyright © 2016 CNES
+   
+   
+   This file is part of the OpenBACH testbed.
+   
+   
+   OpenBACH is a free software : you can redistribute it and/or modify it under the
+   terms of the GNU General Public License as published by the Free Software
+   Foundation, either version 3 of the License, or (at your option) any later
+   version.
+   
+   This program is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY, without even the implied warranty of MERCHANTABILITY or FITNESS
+   FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+   details.
+   
+   You should have received a copy of the GNU General Public License along with
+   this program. If not, see http://www.gnu.org/licenses/.
+   
+   
+   
+   @file     update_job_stat_policy.py
+   @brief    Call the openbach-function update_job_stat_policy
+   @author   Adrien THIBAUD <adrien.thibaud@toulouse.viveris.com>
+"""
 
-"""
-update_job_stat_policy.py - <+description+>
-"""
+
+#!/usr/bin/env python3
 
 import argparse
 from frontend import update_job_stat_policy, date_to_timestamp, pretty_print

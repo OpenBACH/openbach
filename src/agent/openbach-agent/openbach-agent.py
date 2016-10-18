33d2d2423ad1819bc6aa859b59acc0c9f7930929
Adrien Thibaud <adrien.thibaud@toulouse.viveris.com>
2016-10-18 10:19:28 +0200

    Correction of an error

--- src/agent/openbach-agent/openbach-agent.py
+++ src/agent/openbach-agent/openbach-agent.py
@@ -52,7 +52,7 @@ from apscheduler.triggers.date import DateTrigger
 from apscheduler.util import datetime_repr
 
 import psutil
-import rstats_client as rstats
+import rstats_api as rstats
 
 try:
     # Try importing unix stuff

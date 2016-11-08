2c52234916bc705c5ef8b731f21a798845f720e7
Adrien Thibaud <adrien.thibaud@toulouse.viveris.com>
2016-11-08 15:30:45 +0100

    Update of the Job fping

--- src/jobs/layer4/traffic_generator/fping/files/fping.py
+++ src/jobs/layer4/traffic_generator/fping/files/fping.py
@@ -6,29 +6,29 @@
    network/physical entities (under test) and collect data from them. It is
    composed of an Auditorium (HMIs), a Controller, a Collector and multiple
    Agents (one for each network entity that wants to be tested).
-   
-   
+
+
    Copyright © 2016 CNES
-   
-   
+
+
    This file is part of the OpenBACH testbed.
-   
-   
+
+
    OpenBACH is a free software : you can redistribute it and/or modify it under the
    terms of the GNU General Public License as published by the Free Software
    Foundation, either version 3 of the License, or (at your option) any later
    version.
-   
+
    This program is distributed in the hope that it will be useful, but WITHOUT
    ANY WARRANTY, without even the implied warranty of MERCHANTABILITY or FITNESS
    FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
    details.
-   
+
    You should have received a copy of the GNU General Public License along with
    this program. If not, see http://www.gnu.org/licenses/.
-   
-   
-   
+
+
+
    @file     fping.py
    @brief    Sources of the Job fping
    @author   Adrien THIBAUD <adrien.thibaud@toulouse.viveris.com>
@@ -37,39 +37,35 @@
 
 
 import subprocess
-import shlex
 import argparse
 import time
-import sys
 import os
 import syslog
 import rstats_api as rstats
-from subprocess import Popen, PIPE, STDOUT
 
-def get_simple_cmd_output(cmd, stderr=STDOUT):
-    """
-    Execute a simple external command and get its output.
-    """
-    args = shlex.split(cmd)
-    return Popen(args, stdout=PIPE, stderr=stderr).communicate()[0]
+
+def command_line_flag_for_argument(argument, flag):
+    if argument is not None:
+        yield flag
+        yield argument
+
+
+def handle_exception(exception, connection_id, measurement_name, timestamp):
+    statistics = {'status': 'Error'}
+    rstats.send_stat(connection_id, measurement_name, timestamp, **statistics)
+    syslog.syslog(syslog.LOG_ERR, "ERROR: %s" % exception)
 
 
 def main(destination_ip, count, interval, interface, packetsize, ttl, duration):
     conffile = "/opt/openbach-jobs/fping/fping_rstats_filter.conf"
 
-    cmd = 'fping {}'.format(destination_ip)
-    if count:
-        cmd = '{} -c {}'.format(cmd, count)
-    if interval:
-        cmd = '{} -i {}'.format(cmd, interval)
-    if interface:
-        cmd = '{} -I {}'.format(cmd, interface)
-    if packetsize:
-        cmd = '{} -s {}'.format(cmd, packetsize)
-    if ttl:
-        cmd = '{} -t {}'.format(cmd, ttl)
-    if duration:
-        cmd = '{} -w {}'.format(cmd, duration)
+    cmd = ['fping', destination_ip]
+    cmd.extend(command_line_flag_for_argument(str(count), '-c'))
+    cmd.extend(command_line_flag_for_argument(interval, '-i'))
+    cmd.extend(command_line_flag_for_argument(interface, '-I'))
+    cmd.extend(command_line_flag_for_argument(packetsize, '-s'))
+    cmd.extend(command_line_flag_for_argument(ttl, '-t'))
+    cmd.extend(command_line_flag_for_argument(duration, '-w'))
 
     measurement_name = 'fping'
     job_instance_id = int(os.environ.get('INSTANCE_ID', 0))
@@ -77,23 +73,28 @@ def main(destination_ip, count, interval, interface, packetsize, ttl, duration):
     connection_id = rstats.register_stat(conffile, 'fping', job_instance_id,
                                          scenario_instance_id)
     if connection_id == 0:
-        quit()
+        return
 
     while True:
         timestamp = int(round(time.time() * 1000))
         try:
-            rtt_data = get_simple_cmd_output(cmd).strip().decode().split(':')[-1].split('=')[-1].split('/')[1]
-        except Exception as ex:
-            statistics = {'status': 'Error'}
-            rstats.send_stat(connection_id, measurement_name, timestamp, **statistics)
-            syslog.syslog(syslog.LOG_ERR, "ERROR: %s" % ex)
+            output = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError as ex:
+            if ex.returncode in (-15, -9):
+                continue
+            handle_exception(ex, connection_id, measurement_name, timestamps)
+            return
+        try:
+            rtt_data = output.strip().decode().split(':')[-1].split('=')[-1].split('/')[1]
+        except IndexError as ex:
+            handle_exception(ex, connection_id, measurement_name, timestamps)
             return
         statistics = {'rtt': rtt_data}
-        rstats.send_stat(connection_id, measurement_name, timestamp, **statistics)
+        rstats.send_stat(connection_id, measurement_name, timestamp,
+                         **statistics)
 
 
 if __name__ == "__main__":
-    global chain
     # Define Usage
     parser = argparse.ArgumentParser(description='',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -123,3 +124,4 @@ if __name__ == "__main__":
     duration = args.duration
 
     main(destination_ip, count, interval, interface, packetsize, ttl, duration)
+

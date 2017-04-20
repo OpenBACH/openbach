0a58f9f956108dd6619c0e203bb0b2c38c17629c
ozerrouq <oumaima.zerrouq@toulouse.viveris.com>
2017-04-20 13:32:36 +0000

    modification of ip_route.py file

--- src/jobs/user_jobs/transport/ip_route/files/ip_route.py
+++ src/jobs/user_jobs/transport/ip_route/files/ip_route.py
@@ -1,77 +1,46 @@
 import subprocess
 import argparse
-import time
-import syslog
-import collect_agent
 
+
+"""
 def command_line_flag_for_argument(argument, flag):
     if argument is not None:
         yield flag
         yield str(argument)
-
-def handle_exception(exception, timestamp):
+"""
+"""def handle_exception(exception, timestamp):
     statistics = {'status': 'Error'}
     collect_agent.send_stat(timestamp, **statistics)
     collect_agent.send_log(syslog.LOG_ERR, "ERROR: %s" % exception)
+"""
 
-
-def main(destination_ip, count, interval, interface, packetsize, ttl):
+def main(destination_ip, subnet_Mask, Gatewap_ip):
     conffile = "/opt/openbach-jobs/ip_route/ip_route_rstats_filter.conf"
 
     cmd = ['ip_route', destination_ip]
-    cmd.extend(command_line_flag_for_argument(count, '-c'))
-    cmd.extend(command_line_flag_for_argument(interval, '-i'))
-    cmd.extend(command_line_flag_for_argument(interface, '-I'))
-    cmd.extend(command_line_flag_for_argument(packetsize, '-s'))
-    cmd.extend(command_line_flag_for_argument(ttl, '-t'))
+    cmd = ['ip_route', Subnet_Mask]
+    cmd = ['ip_route', Gateway_ip]
 
-    success = collect_agent.register_collect(conffile)
-    if not success:
-        return
 
-    #persitent jobs that only finishes when it is stopped by OpenBACH
-    while True:
-        timestamp = int(round(time.time() * 1000))
-        try:
-            output = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
-        except subprocess.CalledProcessError as ex:
-            if ex.returncode in (-15, -9):
-                continue
-            handle_exception(ex, timestamp)
-            return
-        try:
-            rtt_data = output.strip().decode().split(':')[-1].split('=')[-1].split('/')[1]
-        except IndexError as ex:
-            handle_exception(ex, timestamp)
-            return
-        statistics = {'rtt': rtt_data}
-        collect_agent.send_stat(timestamp, **statistics)
-
+    #non persitent jobs that finishes without being stopped manually
 
 if __name__ == "__main__":
     # Define Usage
     parser = argparse.ArgumentParser(description='',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('destination_ip', metavar='destination_ip', type=str,
+    parser.add_argument('destination_ip', type=int,
                         help='')
-    parser.add_argument('-c', '--count', type=int,
-                        help='', default=3)
-    parser.add_argument('-i', '--interval', type=int,
+    parser.add_argument('Subnet_Mask', type=int,
+                        help='', 
+    parser.add_argument('Gateway_ip', type=int,
                         help='')
-    parser.add_argument('-I', '--interface', type=str,
-                        help='')
-    parser.add_argument('-s', '--packetsize', type=int,
-                        help='')
-    parser.add_argument('-t', '--ttl', type=int,
-                        help='')
+
 
     # get args
     args = parser.parse_args()
     destination_ip = args.destination_ip
-    count = args.count
-    interval = args.interval
-    interface = args.interface
-    packetsize = args.packetsize
-    ttl = args.ttl
+    Subnet_Mask = args.Subnet_Mask
+    Gateway_ip = args.Gateway_ip
+    
 
-    main(destination_ip, count, interval, interface, packetsize, ttl)
+    main(destination_ip, Subnet_Mask, Gateway_ip)

531dedb458a265b8bc460c8b23e8c1ae43eff3bf
ozerrouq <oumaima.zerrouq@toulouse.viveris.com>
2017-05-03 14:38:06 +0000

    Adding new variable action and exception for adding an existing route

--- src/jobs/user_jobs/transport/ip_route/files/ip_route.py
+++ src/jobs/user_jobs/transport/ip_route/files/ip_route.py
@@ -4,20 +4,28 @@ import syslog
 import collect_agent
 
 
-def main(destination_ip, subnet_mask, gateway_ip):
+def main(destination_ip, subnet_mask, gateway_ip, action):
     conffile = "/opt/openbach-jobs/ip_route/ip_route_rstats_filter.conf"
     success = collect_agent.register_collect(conffile)     
     if not success:
         return
 
+
     collect_agent.send_log(syslog.LOG_INFO, "Starting ip_route")
 
+
+
     # Je défini une liste avec mes arguments et je fais ensuite appel à ma liste en utilisant le module subprocess
-    commande = ["route", "add", "-net", destination_ip, "netmask", subnet_mask, "gw", gateway_ip]
-    subprocess.check_call(commande)
 
-    collect_agent.send_log(syslog.LOG_INFO, "ip_route job done") 
+#    commande = ["route", "add", "-net", destination_ip, "netmask", subnet_mask, "gw", gateway_ip]
+#    subprocess.check_call(commande)
+#    collect_agent.send_log(syslog.LOG_INFO, "ip_route job done") 
 
+
+
+
+
+#Fonction de définition du type ip :
 
 def ip(argument):
     address = argument.split('.')
@@ -31,7 +39,34 @@ def ip(argument):
     return argument
 
 
+
+
+
+
+    # Adding a new variable "action" for the addition/deletion of a route
+
+    if action == 1 :
+	#Add a route
+	try:
+	    subprocess.call(["route", "add", "-net", destination_ip, "netmask", subnet_mask, "gw", gateway_ip])
+	    collect_agent.send_log(syslog.LOG_DEBUG, "New Route Added")
+ 	except Exception as ex:
+            collect_agent.send_log(syslog.LOG_ERR, "ERROR Adding the route " + str(ex))
+
+    else:
+	#Delete a route
+	try:
+	    subprocess.call(["route", "del", "-net", destination_ip, "netmask", subnet_mask, "gw", gateway_ip])
+	    collect_agent.send_log(syslog.LOG_DEBUG, "Route deleted")
+
+
+
+
+
+
+
 if __name__ == "__main__":
+
     # Define Usage
     parser = argparse.ArgumentParser(description='',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)

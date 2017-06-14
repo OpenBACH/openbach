64c22d21ca50adc78a4202d9b4bfdb931f72f063
ozerrouq <oumaima.zerrouq@toulouse.viveris.com>
2017-06-14 14:35:39 +0000

    Write job file (Add address split)

--- src/jobs/user_jobs/transport/net_create/files/net_create.py
+++ src/jobs/user_jobs/transport/net_create/files/net_create.py
@@ -21,53 +21,55 @@ def ip(argument):
     return argument
 
 
-def main(network_name, address, dhcp):
-    conffile = "/opt/openbach-jobs/ip_route/ip_route_rstats_filter.conf"
+def main(net_name, address):
+    conffile = "/opt/openbach-jobs/net_create/net_create_rstats_filter.conf"
     success = collect_agent.register_collect(conffile)     
     if not success:
         return
 
     collect_agent.send_log(syslog.LOG_ERR, "Stating net_create")
 
-    # Create template
-    try:
-        subprocess.check_call(["vim", net_name, ".yml"])
-        collect_agent.send_log(syslog.LOG_DEBUG, "New network template added")
-        
-# là j demande au user les valeurs des parametres
-     with open(''network_name'.yml', 'a') as file :
-         file.writelines('heat_template_version: 2013-05-23\n' + 'description:
-                         Simple template to deploy a single network instance\n'
-                         + ' ' + network_name + ':\n' + '  properties:\n' +
-                         '     name:' + network_name + '\n'+ '     shared:
-                         false\n'+'   type: OS::Neutron::Net\n\n'+ '
-                         network_subnet':\n' + '   properties:\n'+ '
-                         allocation_pools:\n' +'      - end: 192.172.12.254_n'+
-                         start: 192.172.12.2\n' +'      cidr:'+ address
-                         +'/24\n'+'     dns_nameservers: []\n+
-                         enable_dhcp:'+dhcp\n+'     host_routes: []\n+
-                         ip_version: 4\n+'     name: '+ network_name\n+'
-                         network_id:\n'+'       get_resource: '+network_name+\n'
-                         +'    type: OS::Neutron::Subnet')
+    
+    # Define variable net for pool address allocation
+
+#    address.split(".")
+#    net = var[0] + "." + var[1] + "." + var[2]
+    address.rsplit(".",1)
+    net = address.rsplit(".",1)
+    
+    # CREATE TEMPLATE
+
+    with open(net_name + '.yml', 'w') as file :
+        file.writelines('heat_template_version: 2013-05-23\n\n' + 'description:'
+                        + ' Simple template to deploy a single network instance\n\n' + 'resources'+':\n\n' +  net_name + ':\n' + 
+                        '  properties:\n' + '     admin_state_up: true\n' + 
+                        '     name: ' + net_name + '\n'+ '     shared: false\n'+'  type: OS::Neutron::Net\n\n'+
+                        'network_subnet:\n' + '  properties:\n'+
+                        '    allocation_pools:\n' + '    - end: ' + net[0] + '.127\n' +
+                        '      start: ' + net[0] + '.2\n' + '    cidr: ' + address +
+                        '/24\n' + '    dns_nameservers: []\n' +
+                        '    enable_dhcp: true' + '\n' + '    host_routes: []\n' +
+                        '    ip_version: 4' + '\n' + '    name: '+ net_name + '\n' +
+                        '    network_id:\n' + '      get_resource: ' + net_name +
+                        '\n' + '  type: OS::Neutron::Subnet')
+
    # Create network
 
-     subprocess.check_call(["heat stack-create", network_name, "–template", 
-                            "network_name",".yml"])
-     collect_agent.send_log(syslog.LOG_DEBUG, "New network added")
+#    subprocess.check_call(["heat stack-create", net_name, "–template", 
+#                           net_name + "yml"])
+#    collect_agent.send_log(syslog.LOG_DEBUG, "New network added")
 
 if __name__ == "__main__":
-
+    
     # Define Usage
     parser = argparse.ArgumentParser(description='',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-n','--network_name', type=str, help='')
+    parser.add_argument('-n','--net_name', type=str, help='')
     parser.add_argument('-a','--address', type=ip, help='') 
-    parser.add_argument('-d','--dhcp', type=int, help='')
 
     # get args
     args = parser.parse_args()
-    network_name = args.network_name
-    address = args.subnet_address
-    dhcp = args.dhcp
+    net_name = args.net_name
+    address = args.address
 
-    main(network_name, address, dhcp)
+    main(net_name, address)

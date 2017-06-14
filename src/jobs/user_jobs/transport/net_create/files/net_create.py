7d970843a12744785e5af84ed71fda05114f137d
ozerrouq <oumaima.zerrouq@toulouse.viveris.com>
2017-06-14 10:28:21 +0000

    net_create : writing job file

--- src/jobs/user_jobs/transport/net_create/files/net_create.py
+++ src/jobs/user_jobs/transport/net_create/files/net_create.py
@@ -21,87 +21,53 @@ def ip(argument):
     return argument
 
 
-def main(destination_ip, subnet_mask, gateway_ip, action, default_gateway, default_gw_name):
+def main(network_name, address, dhcp):
     conffile = "/opt/openbach-jobs/ip_route/ip_route_rstats_filter.conf"
     success = collect_agent.register_collect(conffile)     
     if not success:
         return
 
-    collect_agent.send_log(syslog.LOG_ERR, "Starting ip_route")
-#   collect_agent.send_log(syslog.LOG_INFO,== c'etait log info avant 
+    collect_agent.send_log(syslog.LOG_ERR, "Stating net_create")
 
+    # Create template
+    try:
+        subprocess.check_call(["vim", net_name, ".yml"])
+        collect_agent.send_log(syslog.LOG_DEBUG, "New network template added")
+        
+# là j demande au user les valeurs des parametres
+     with open(''network_name'.yml', 'a') as file :
+         file.writelines('heat_template_version: 2013-05-23\n' + 'description:
+                         Simple template to deploy a single network instance\n'
+                         + ' ' + network_name + ':\n' + '  properties:\n' +
+                         '     name:' + network_name + '\n'+ '     shared:
+                         false\n'+'   type: OS::Neutron::Net\n\n'+ '
+                         network_subnet':\n' + '   properties:\n'+ '
+                         allocation_pools:\n' +'      - end: 192.172.12.254_n'+
+                         start: 192.172.12.2\n' +'      cidr:'+ address
+                         +'/24\n'+'     dns_nameservers: []\n+
+                         enable_dhcp:'+dhcp\n+'     host_routes: []\n+
+                         ip_version: 4\n+'     name: '+ network_name\n+'
+                         network_id:\n'+'       get_resource: '+network_name+\n'
+                         +'    type: OS::Neutron::Subnet')
+   # Create network
 
-
-
-# """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-    # Je défini une liste avec mes arguments et je fais ensuite appel à ma liste en utilisant le module subprocess
-
-#    commande = ["route", "add", "-net", destination_ip, "netmask", subnet_mask, "gw", gateway_ip]
-#    subprocess.check_call(commande)
-#    collect_agent.send_log(syslog.LOG_INFO, "ip_route job done") 
-
-# """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-
-
-
-
-    # Adding a new variable "action" for the addition/deletion of a route
-    if action == 1:
-       if default_gateway == 1:
-           #Add a default gateway
-           try:
-               subprocess.check_call(["route", "add", "default", "gw", destination_ip, default_gw_name])
-               collect_agent.send_log(syslog.LOG_DEBUG, "New default route added")
-           except Exception as ex:
-               collect_agent.send_log(syslog.LOG_ERR, "ERROR" + str(ex))
-       else:
-           #Add a normal route
-           try:
-               subprocess.check_call(["route", "add", "-net", destination_ip, "netmask", subnet_mask, "gw", gateway_ip])
-               collect_agent.send_log(syslog.LOG_DEBUG,  "Route Added")
-           except Exception as ex:
-               collect_agent.send_log(syslog.LOG_ERR, "ERROR" + str(ex))
-
-    else:
-       if  default_gateway == 1:
-           #delete a default gateway
-           try:
-               subprocess.check_call(["route", "del", "default", "gw", destination_ip, default_gw_name])
-               collect_agent.send_log(syslog.LOG_DEBUG, "Default Route deleted")
-           except Exception as ex:
-               collect_agent.send_log(syslog.LOG_ERR, "ERROR" + str(ex))
-       else:
-           #Delete a normal route
-           try:
-               subprocess.check_call(["route", "del", "-net", destination_ip, "netmask", subnet_mask, "gw", gateway_ip])
-               collect_agent.send_log(syslog.LOG_DEBUG, "Route deleted")
-           except Exception as ex:
-               collect_agent.send_log(syslog.LOG_ERR, "ERROR" + str(ex))
-
-
-#c'etait des subprocess.call= n'envoyaients pas des logs
-
+     subprocess.check_call(["heat stack-create", network_name, "–template", 
+                            "network_name",".yml"])
+     collect_agent.send_log(syslog.LOG_DEBUG, "New network added")
 
 if __name__ == "__main__":
 
     # Define Usage
     parser = argparse.ArgumentParser(description='',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-i','--destination_ip', type=ip, help='')
-    parser.add_argument('-s','--subnet_mask', type=ip, help='') 
-    parser.add_argument('-g','--gateway_ip', type=ip, help='')
-    parser.add_argument('-a', '--action', type=int, help='')
-    parser.add_argument('-d', '--default_gateway', type=int, help='')
-    parser.add_argument('-b', '--default_gw_name', type=str, help='')
-
+    parser.add_argument('-n','--network_name', type=str, help='')
+    parser.add_argument('-a','--address', type=ip, help='') 
+    parser.add_argument('-d','--dhcp', type=int, help='')
 
     # get args
     args = parser.parse_args()
-    destination_ip = args.destination_ip
-    subnet_mask = args.subnet_mask
-    gateway_ip = args.gateway_ip
-    action = args.action
-    default_gateway = args.default_gateway
-    default_gw_name = args.default_gw_name
+    network_name = args.network_name
+    address = args.subnet_address
+    dhcp = args.dhcp
 
-    main(destination_ip, subnet_mask, gateway_ip, action, default_gateway, default_gw_name)
+    main(network_name, address, dhcp)

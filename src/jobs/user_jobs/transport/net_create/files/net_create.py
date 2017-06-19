9711bda4a19725f3c6565c0804ce2cd1b08c40b6
ozerrouq <oumaima.zerrouq@toulouse.viveris.com>
2017-06-19 09:46:19 +0000

    Add argument rcfile

--- src/jobs/user_jobs/transport/net_create/files/net_create.py
+++ src/jobs/user_jobs/transport/net_create/files/net_create.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 # Author:  / <@toulouse.viveris.com>
 
+import os
 import subprocess
 import argparse
 import syslog
@@ -21,7 +22,7 @@ def ip(argument):
     return argument
 
 
-def main(net_name, address, password):
+def main(net_name, address, password, RCfile):
     conffile = "/opt/openbach-jobs/net_create/net_create_rstats_filter.conf"
     success = collect_agent.register_collect(conffile)     
     if not success:
@@ -39,27 +40,69 @@ def main(net_name, address, password):
     
     # CREATE TEMPLATE
 
-    with open(net_name + '.yml', 'w') as file :
-        file.writelines('heat_template_version: 2013-05-23\n\n' + 'description:'
-                        + ' Simple template to deploy a single network instance\n\n' + 'resources'+':\n\n' + ' ' + net_name + ':\n' + 
-                        ' ' + '  properties:\n' + ' '+'     admin_state_up: true\n' + 
-                        ' ' + '     name: ' + net_name + '\n'+' '+ '     shared: false\n'+' '+'  type: OS::Neutron::Net\n\n '+
-                        'network_subnet:\n' +' '+ '  properties:\n'+' '+
-                        '    allocation_pools:\n' +' '+ '    - end: ' + net[0] +
-                        '.127\n' +' '+ '      start: ' + net[0] + '.2\n' +' '+ '    cidr: ' + address +
-                        '/24\n' +' '+ '    dns_nameservers: []\n' +' '+
-                        '    enable_dhcp: true' + '\n' +' '+ '    host_routes: []\n' +' '+
-                        '    ip_version: 4' + '\n ' +'    name: '+ net_name + '\n ' +
-                        '    network_id:\n ' + '      get_resource: ' + net_name +
-                        '\n ' + '  type: OS::Neutron::Subnet')
+    with open("/tmp/" + net_name + '.yml', 'w') as file :
+        file.writelines('''\
+heat_template_version: 2013-05-23
+                        
+description: Simple template to deploy a single network instance
+                        
+resources:
+                        
+ {0}:
+   properties:
+     admin_state_up: true
+     name: {0}
+     shared: false
+   type: OS::Neutron::Net
+                        
+ network_subnet:
+   properties:
+     allocation_pools:
+     - end: {1[0]}.127
+       start: {1[0]}.2
+     cidr: {2}/24
+     dns_nameservers: []
+     enable_dhcp: true
+     host_routes: []
+     ip_version: 4
+     name: {0}
+     network_id:
+       get_resource: {0}
+   type: OS::Neutron::Subnet'''.format(net_name, net, address))
+        
+        
+        # file.writelines('heat_template_version: 2013-05-23\n\n' + 'description:'
+                        # + ' Simple template to deploy a single network instance\n\n' + 'resources'+':\n\n' + ' ' + net_name + ':\n' + 
+                        # ' ' + '  properties:\n' + ' '+'     admin_state_up: true\n' + 
+                        # ' ' + '     name: ' + net_name + '\n'+' '+ '     shared: false\n'+' '+'  type: OS::Neutron::Net\n\n '+
+                        # 'network_subnet:\n' +' '+ '  properties:\n'+' '+
+                        # '    allocation_pools:\n' +' '+ '    - end: ' + net[0] +
+                        # '.127\n' +' '+ '      start: ' + net[0] + '.2\n' +' '+ '    cidr: ' + address +
+                        # '/24\n' +' '+ '    dns_nameservers: []\n' +' '+
+                        # '    enable_dhcp: true' + '\n' +' '+ '    host_routes: []\n' +' '+
+                        # '    ip_version: 4' + '\n ' +'    name: '+ net_name + '\n ' +
+                        # '    network_id:\n ' + '      get_resource: ' + net_name +
+                        # '\n ' + '  type: OS::Neutron::Subnet')
 
    # Create network
     try:
-        
-        subprocess.check_call(["source /home/exploit/CNES-openrc.sh"])
-#        subprocess.check_call(["heat", "stack-create", net_name, "-f", "test1.yml"])
-        subprocess.check_call(["heat","stack-create" , net_name, "-f", net_name + ".yml"])
-        collect_agent.send_log(syslog.LOG_DEBUG, "New network added")
+        # cmd = "echo " +  password + "|" + "source /home/exploit/CNES-openrc.sh"
+        # print(cmd)
+        # subprocess.check_call(cmd, shell=True, executable='/bin/bash')
+        # cmd2 = "heat" " stack-create " + net_name + " -f " + "/tmp/" + net_name + ".yml"
+        # print (cmd2)
+        #subprocess.check_call(["heat", "stack-create", net_name, "-f", "test1.yml"])
+        #subprocess.check_call(cmd2, shell=True, executable='/bin/bash')
+
+        print("vri")
+        subprocess.check_call('echo "{0}" | source /tmp/CNES-openrc.sh '
+                              '&& heat stack-create {1} -f /tmp/{1}.yml'
+                              .format(password, net_name))
+    #Add path to the rc file : 
+    #    subprocess.check_call('echo "{0}" | source /tmp/{2}.sh '
+    #                          '&& heat stack-create {1} -f /tmp/{1}.yml'
+    #                          .format(password, net_name, RCfile))
+        collect_agent.send_log(syslog.LOG_DEBUG, "New network stack added")
     except Exception as ex:
         print(ex)
         collect_agent.send_log(syslog.LOG_ERR, "ERROR" + str(ex))
@@ -72,11 +115,12 @@ if __name__ == "__main__":
     parser.add_argument('-n','--net_name', type=str, help='')
     parser.add_argument('-a','--address', type=ip, help='') 
     parser.add_argument('-p','--password', type=str, help='') 
-    
+    parser.add_argument('-f','--RCfile', type=str, help='')
     # get args
     args = parser.parse_args()
     net_name = args.net_name
     address = args.address
     password = args.password
-
-    main(net_name, address, password)
+    RCfile = args.RCfile
+    
+    main(net_name, address, password, RCfile)

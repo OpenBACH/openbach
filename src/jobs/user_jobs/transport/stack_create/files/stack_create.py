8249d676473d6cdbe8311219d89b656a4304f1dc
ozerrouq <oumaima.zerrouq@toulouse.viveris.com>
2017-06-19 14:57:31 +0000

    Job Stack_create : add password parameter + Create template OK

--- src/jobs/user_jobs/transport/stack_create/files/stack_create.py
+++ src/jobs/user_jobs/transport/stack_create/files/stack_create.py
@@ -22,41 +22,39 @@ def ip(argument):
     return argument
 
 
-def main(stack_name, flavor, image_id, network_name):
+def main(stack_name, flavor, image_id, network, password):
     conffile = "/opt/openbach-jobs/stack_create/stack_create_rstats_filter.conf"
     success = collect_agent.register_collect(conffile)     
     if not success:
         return
 
-    collect_agent.send_log(syslog.LOG_ERR, "Stating stack_create")
+    collect_agent.send_log(syslog.LOG_ERR, "Starting stack_create")
 
     
     # CREATE TEMPLATE
     
 
-    with open("/tmp/" + net_name + '.yml', 'w') as file :
+    with open("/tmp/" + stack_name + '.yml', 'w') as file :
         file.writelines('''\
 heat_template_version: 2015-04-30
                         
 description: Simple template to deploy a single compute instance
                         
 resources:
-  {0}                        
+  {0}:                        
     type: OS::Neutron::Server
     properties:
       image : {1}
       flavor: {2}
       networks:
-        -network:{3}
-        -network:'''.format(stack_name, image_id, flavor, network))
+        -network: {3}'''.format(stack_name, image_id, flavor, network))
         
 
    # Create stack
     try:
-        print("vri")
         subprocess.check_call('echo "{0}" | source /tmp/CNES-openrc.sh '
                               '&& heat stack-create {1} -f /tmp/{1}.yml'
-                              .format(password, net_name))
+                              .format(password, stack_name))
         collect_agent.send_log(syslog.LOG_DEBUG, "New stack added")
     except Exception as ex:
         print(ex)
@@ -70,10 +68,11 @@ if __name__ == "__main__":
     # Define Usage
     parser = argparse.ArgumentParser(description='',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-n','--stack_name', type=str, help='')
+    parser.add_argument('-s','--stack_name', type=str, help='')
     parser.add_argument('-f','--flavor', type=str, help='') 
-    parser.add_argument('-n','--image_id', type=str, help='')
-    parser.add_argument('-n','--network_name', type=str, help='')
+    parser.add_argument('-i','--image_id', type=str, help='')
+    parser.add_argument('-n','--network', type=str, help='')
+    parser.add_argument('-p','--password', type=str, help='')
 
     # get args
     args = parser.parse_args()
@@ -81,5 +80,6 @@ if __name__ == "__main__":
     flavor = args.flavor
     image_id = args.image_id
     network = args.network
+    password = args.password
 
-    main(stack_name, flavor, image_id, network)
+    main(stack_name, flavor, image_id, network, password)

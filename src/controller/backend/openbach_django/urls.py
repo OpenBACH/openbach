55d21abdd17a90df044c2a8d67822a1624bd1209
David Pradas <david.pradas@toulouse.viveris.com>
2018-02-01 13:22:44 +0000

    correct bug on urls.py

--- src/controller/backend/openbach_django/urls.py
+++ src/controller/backend/openbach_django/urls.py
@@ -92,7 +92,8 @@ urlpatterns = [
         name='scenario_view'),
 
     url(r'^scenario/(?P<scenario_name>[^/]+)/scenario_instance/?',
-        views.ScenarioInstancesView.as_view(), name='scenario_scenario_instance_view')
+        views.ScenarioInstancesView.as_view(),
+        name='scenario_scenario_instance_view'),
     url(r'^scenario_instance/?$', views.ScenarioInstancesView.as_view(),
         name='scenario_instances_view'),
     url(r'^scenario_instance/(?P<id>[^/]+)/?$',
